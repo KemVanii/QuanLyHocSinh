ed1e036daeec36df78ae7c398a7ac2de2672d78e
Kemvaniii <redok1706@gmail.com>
2023-12-29 00:22:22 +0700

    updated models.py

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from flask_login import UserMixin
-from sqlalchemy import Column, Integer, Double, String, ForeignKey, Enum, DateTime, Boolean
+from sqlalchemy import Column, Integer, Float, String, ForeignKey, Enum, DateTime, Boolean
 from sqlalchemy.orm import relationship
-from app import db
+from app import app, db
 import enum
 
 
@@ -12,21 +12,21 @@ class UserRoleEnum(enum.Enum):
     Admin = 4
 
 
-class User(db.Model, UserMixin):
+class Person(db.Model):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(10), nullable=False)
     gender = Column(Boolean, nullable=False)
     dob = Column(DateTime, nullable=False)
     address = Column(String(100), nullable=False)
-    username = Column(String(20), nullable=False, unique=True)
+    phones = relationship('Phone', backref='person', lazy=True)
+
+
+class User(db.Model, UserMixin):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False, unique=True)
+    username = Column(String(50), nullable=False, unique=True)
     password = Column(String(100), nullable=False)
     user_role = Column(Enum(UserRoleEnum))
-    score_boards = relationship('Score_board', 'user', lazy=True)
-    phones = relationship('Phone', 'user', lazy=True)
-
-
-class Teacher(User):
-    classes = relationship('Class', backref='teacher', lazy=True)
 
 
 class Khoi(db.Model):
@@ -39,60 +39,63 @@ class Class(db.Model):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(10), nullable=False)
     size = Column(Integer, nullable=False)
-    Khoi_id = Column(Integer, ForeignKey(Khoi.id))
-    teacher_id = Column(Integer, ForeignKey(Teacher.id))
-    students = relationship('User', backref='class', lazy=True)
+    Khoi_id = Column(Integer, ForeignKey(Khoi.id), nullable=False)
+    teacher_id = Column(Integer, ForeignKey(Person.id), nullable=False)
+    students = relationship('Student', foreign_keys='Student.class_id', backref='class', lazy=True)
+
+
+class Teacher(Person):
+    classes = relationship('Class', foreign_keys='Class.teacher_id', backref='teacher', lazy=True)
 
 
 class Phone(db.Model):
     id = Column(Integer, primary_key=True, autoincrement=True)
     number = Column(String(10), nullable=False)
-    user_id = Column(Integer, ForeignKey(User.id))
+    person_id = Column(Integer, ForeignKey(Person.id), nullable=False)
 
 
 class Subject(db.Model):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(10), nullable=False)
-    score_boards = relationship('Score_board', 'subject', lazy=True)
+    score_boards = relationship('ScoreBoard',  backref='subject', lazy=True)
 
 
 class Semester(db.Model):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(10), nullable=False)
     # HK1_23
-    score_boards = relationship('Score_board', backref='semester', lazy=True)
+    score_boards = relationship('ScoreBoard', backref='semester', lazy=True)
 
 
 class ScoreBoard(db.Model):
     id = Column(Integer, primary_key=True, autoincrement=True)
     scores = relationship('Score', backref='score_board', lazy=True)
-    student_id = Column(Integer, ForeignKey(User.id))
-    subject_id = Column(Integer, ForeignKey(Subject.id))
-    semester_id = Column(Integer, ForeignKey(Semester.id))
+    student_id = Column(Integer, ForeignKey(Person.id), nullable=False)
+    subject_id = Column(Integer, ForeignKey(Subject.id), nullable=False)
+    semester_id = Column(Integer, ForeignKey(Semester.id), nullable=False)
+
+
+class Student(Person):
+    class_id = Column(Integer, ForeignKey(Class.id))
+    score_boards = relationship('ScoreBoard', backref='student', lazy=True)
 
 
 class Score(db.Model):
     id = Column(Integer, primary_key=True, autoincrement=True)
-    value = Column(Double)
+    value = Column(Float)
     type = Column(String(20), nullable=False)
     score_board_id = Column(Integer, ForeignKey(ScoreBoard.id))
 
 
-class Student(User):
-    class_id = Column(Integer, ForeignKey(Class.id))
-    score_boards_id = Column(Integer, ForeignKey(ScoreBoard.id))
-
-
 if __name__ == '__main__':
-    from app import app
-
     with app.app_context():
         db.create_all()
-    import hashlib
 
-    u = User(name='Admin', username='admin',
-             password=str(hashlib.md5('12345'.encode('utf-8')).hexdigest()),
-             user_role=UserRoleEnum.Admin)
+        import hashlib
 
-    db.session.add(u)
-    db.session.commit()
+        u = User(name='Admin', username='admin',
+                 password=str(hashlib.md5('123456'.encode('utf-8')).hexdigest()),
+                 user_role=UserRoleEnum.Admin)
+
+        db.session.add(u)
+        db.session.commit()
