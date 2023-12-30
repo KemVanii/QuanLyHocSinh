f6ff1cdd82492eced54b4a5499b70abfab118693
Kemvaniii <redok1706@gmail.com>
2023-12-30 13:53:57 +0700

    cap nhat login.html, theml qui dinh html

--- app/index.py
+++ app/index.py
@@ -6,6 +6,7 @@ from app import app, login
 from app.models import *
 import dao
 
+
 @login.user_loader
 def loader_user(user_id):
     return User.query.get(user_id)
@@ -29,52 +30,60 @@ def login():
             return redirect(url_for("index"))
 
     return render_template("login.html")
+
+
 @app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for("index"))
+
 
 @app.route('/tiepnhanhocsinh')
 def tiepNhanHocSinh():
     funcs = []
     if current_user.is_authenticated:
         funcs = dao.load_function(current_user.user_role)
-    return render_template("tiepNhanHocSinh.html",funcs=funcs)
+    return render_template("tiepNhanHocSinh.html", funcs=funcs)
+
 
 @app.route('/lapdanhsach')
 def lapDanhSach():
     funcs = []
     if current_user.is_authenticated:
         funcs = dao.load_function(current_user.user_role)
-    return render_template("lapDanhSach.html",funcs=funcs)
+    return render_template("lapDanhSach.html", funcs=funcs)
+
 
 @app.route('/dieuchinhdanhsach')
 def dieuChinhDanhSach():
     funcs = []
     if current_user.is_authenticated:
         funcs = dao.load_function(current_user.user_role)
-    return render_template("dieuChinhDanhSach.html",funcs=funcs)
+    return render_template("dieuChinhDanhSach.html", funcs=funcs)
+
 
 @app.route('/quydinh')
 def quyDinh():
     funcs = []
     if current_user.is_authenticated:
         funcs = dao.load_function(current_user.user_role)
-    return render_template("quyDinh.html",funcs=funcs)
+    return render_template("quyDinh.html", funcs=funcs)
+
 
 @app.route('/thongke')
 def thongKe():
     funcs = []
     if current_user.is_authenticated:
         funcs = dao.load_function(current_user.user_role)
-    return render_template("thongKe.html",funcs=funcs)
+    return render_template("thongKe.html", funcs=funcs)
+
 
 @app.route('/diem')
 def diem():
     funcs = []
     if current_user.is_authenticated:
         funcs = dao.load_function(current_user.user_role)
-    return render_template("diem.html",funcs=funcs)
+    return render_template("diem.html", funcs=funcs)
 
 
 if __name__ == '__main__':
