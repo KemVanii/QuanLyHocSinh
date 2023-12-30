696fddb283893bdd48871bb8ba0c2c3bc3b2f062
TuPham2803 <tupham2803@gmail.com>
2023-12-30 09:32:22 +0700

    fix: switch case not support in python 3.9

--- app/dao.py
+++ app/dao.py
@@ -1,40 +1,39 @@
 from app.models import UserRoleEnum
 
 def load_function(user_role):
-    match user_role:
-        case UserRoleEnum.Employee:
-            return [
-                {
-                    'name': 'Tiếp nhận học sinh',
-                    'url': '/tiepnhanhocsinh'
-                },
-                {
-                    'name': 'Lập danh sach',
-                    'url': '/lapdanhsach'
-                },
-                {
-                    'name': 'Điều chỉnh danh sách',
-                    'url': '/dieuchinhdanhsach'
-                },
-            ]
-        case UserRoleEnum.Teacher:
-            return [
-                {
-                    'name': 'Điểm',
-                    'url': '/diem'
-                }
-            ]
-        case UserRoleEnum.Admin:
-            return [
-                {
-                    'name': 'Quy định',
-                    'url': '/quydinh'
-                },
-                {
-                    'name': 'Thống kê',
-                    'url': '/thongke'
-                }
-            ]
+    if user_role == UserRoleEnum.Employee:
+        return [
+            {
+                'name': 'Tiếp nhận học sinh',
+                'url': '/tiepnhanhocsinh'
+            },
+            {
+                'name': 'Lập danh sach',
+                'url': '/lapdanhsach'
+            },
+            {
+                'name': 'Điều chỉnh danh sách',
+                'url': '/dieuchinhdanhsach'
+            },
+        ]
+    elif user_role == UserRoleEnum.Teacher:
+        return [
+            {
+                'name': 'Điểm',
+                'url': '/diem'
+            }
+        ]
+    elif user_role == UserRoleEnum.Admin:
+        return [
+            {
+                'name': 'Quy định',
+                'url': '/quydinh'
+            },
+            {
+                'name': 'Thống kê',
+                'url': '/thongke'
+            }
+        ]
     return []
 
 # read json and write json
