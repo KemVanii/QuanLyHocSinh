0cbf47e68ed88bd0315c91829fb3a7420a4a24ba
Kemvaniii <redok1706@gmail.com>
2024-01-06 18:55:44 +0700

    add modify subjects, users

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from urllib.parse import quote
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+import json
+import os
 
 app = Flask(__name__)
 
@@ -10,8 +12,14 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/quanl
     'Admin@123')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config["SQLALCHEMY_RECORD_QUERIES"] = True
-# app.config["maxClassSize"] = int(dao.readjson()['maxClassSize'])
 
+json_file_path = os.path.join(app.root_path, 'static', 'policies.json')
+with open(json_file_path, 'r') as file:
+    data = json.load(file)
+
+# Update app.config with data from the JSON file
+for key, value in data.items():
+    app.config[key] = value
 
 db = SQLAlchemy(app=app)
 login = LoginManager()
--- app/admin.py
+++ app/admin.py
@@ -1,25 +1,53 @@
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+from flask_login import current_user
+from sqlalchemy import inspect
 from app.models import *
 from app import app, db
+from app.models import Subject
+import hashlib
 
 admin = Admin(app=app, name='Admin', template_mode='bootstrap4', url="/")
 
 
-class MyStudent(ModelView):
+class AuthenticatedEmployee(ModelView):
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.user_role == UserRoleEnum.Employee
+
+
+class MyStudent(AuthenticatedEmployee):
     column_searchable_list = ['name', 'id']
     column_editable_list = ['name', 'dob', 'address', 'gender']
     edit_modal = True
 
-class MyUser(ModelView):
-    # column_searchable_list = ['name', 'id']
-    # column_editable_list = ['name', 'dob', 'address', 'gender']
+
+class AuthenticatedAdmin(ModelView):
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.user_role == UserRoleEnum.Admin
+
+
+class MyUser(AuthenticatedAdmin):
+    edit_modal = True
+    column_hide_backrefs = False
+    column_filters = ['user_role']
+    column_list = [c_attr.key for c_attr in inspect(User).mapper.column_attrs]
+
+    def on_model_change(self, form, model, is_created):
+        # Hash the password before saving to the database
+        if 'password' in form:
+            password = form.password.data
+            model.password = hashlib.md5(password.encode('utf-8')).hexdigest()
+
+
+class MySubject(AuthenticatedAdmin):
     edit_modal = True
 
-class MySubject(ModelView):
-    # column_searchable_list = ['name', 'id']
-    # column_editable_list = ['name', 'dob', 'address', 'gender']
-    edit_modal = True
+    def on_model_delete(self, model):
+        new_parent = Subject.query.first()
+        User.query.filter_by(subject_id=model.id).update({'subject_id': new_parent.id})
+        db.session.commit()
+
+
 admin.add_view(MyStudent(Student, db.session))
 admin.add_view(MyUser(User, db.session))
 admin.add_view(MySubject(Subject, db.session))
