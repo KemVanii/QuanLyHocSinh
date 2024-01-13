279ac863f8231c4e23012a7d6ac96405c714ecc4
TuPham2803 <tupham2803@gmail.com>
2024-01-13 23:46:45 +0700

    add load when send mail

--- app/static/js/xemdiemlop.js
+++ app/static/js/xemdiemlop.js
@@ -1,8 +1,10 @@
 const btnSendMail = document.querySelector('#btnSendMail');
-
+const  btnLoadSendMail = document.querySelector('#btnLoadSendMail');
 btnSendMail.addEventListener('click', async function () {
   const userConfirmed = confirm('Xác nhận gửi?');
   if (userConfirmed) {
+    btnSendMail.classList.toggle('d-none');
+    btnLoadSendMail.classList.toggle('d-none');
     const idLop = document.querySelector('#inputIdLop').value;
     const hk = document.querySelector('#inputIdHocKy').value;
     const formData = new FormData();
@@ -18,7 +20,8 @@ btnSendMail.addEventListener('click', async function () {
       }
       const data = await response.json();
       console.log(data);
-
+      btnSendMail.classList.toggle('d-none');
+      btnLoadSendMail.classList.toggle('d-none');
       alert('Gửi email thành công!');
     } catch (error) {
       console.error('There was a problem with the fetch operation:', error);
